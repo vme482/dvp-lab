d40ea5156cbb29018b7610ad7a85d493689b138e
vme482 <vmegha343@gmail.com>
2024-10-16 00:34:49 -0700

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+std=['sslc','puc','1sem','2sem']
+values=[500,600,650,690]
+plt.bar(std,values,color='purple')
+plt.xlabel("Education",fontsize=12)
+plt.ylabel("Marks obtanied",fontsize=12)
+plt.title("varsha s 1KI23CS176")
+plt.show()
