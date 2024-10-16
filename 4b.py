1a3ec05055e21af0166144ae6b2fc6915a4dbcc3
vme482 <vmegha343@gmail.com>
2024-10-16 00:35:47 -0700

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[-1,0,1,2,3]
+plt.scatter(x,y)
+plt.scatter(x,y,color='purple')
+plt.xlabel("x-axis",fontsize=12)
+plt.ylabel("y-axis",fontsize=12)
+plt.title("varsha s 1KI23CS176")
+plt.show()
